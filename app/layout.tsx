6bea566203e6a58bd4d895ee0d16f1f614aaf73a
Jinse <wlstp8473@khu.ac.kr>
2026-03-16 12:51:52 +0900

    step 7

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,9 +29,9 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <header>
-          <nav>
-            <a href="/">메인</a>
-            <a href="/list">목록</a>
+          <nav className="flex gap-4">
+            <Link href="/">메인</Link>
+            <Link href="/list">목록</Link>
           </nav>
         </header>
         {children}
