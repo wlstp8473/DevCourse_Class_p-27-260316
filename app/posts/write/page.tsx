447c1439cb84bc8d0bbe25b73148d85800ad7ea9
Jinse <wlstp8473@khu.ac.kr>
2026-03-17 14:52:28 +0900

    step 22-1

--- app/posts/write/page.tsx
+++ app/posts/write/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 export default function Write() {
 
     const onSubmitHandler = (e: any) => {
@@ -18,19 +19,33 @@ export default function Write() {
             content.focus();
             return;
         }
+
+        // db에 저장.
+        fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/v1/posts`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                "title": title.value,
+                "content": content.value
+            })
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+            })
     }
 
     return (
         <>
-            <div className="flex flex-col gap-4 items-center">
-                <h1>글 작성</h1>
+            <h1>글 작성</h1>
 
-                <form action="" onSubmit={onSubmitHandler} className="flex flex-col gap-4">
-                    <input type="text" name="title" className="border-1 rounded p-2" placeholder="제목을 입력해주세요" />
-                    <textarea rows={10} name="content" className="border-1 rounded p-2" placeholder="내용을 입력해주세요"></textarea>
-                    <input type="submit" value="작성" className="border-1 rounded p-2" />
-                </form>
-            </div>
+            <form action="" onSubmit={onSubmitHandler} className="flex flex-col gap-4">
+                <input type="text" name="title" className="border-1 rounded p-2" placeholder="제목을 입력해주세요" />
+                <textarea rows={10} name="content" className="border-1 rounded p-2" placeholder="내용을 입력해주세요"></textarea>
+                <input type="submit" value="작성" className="border-1 rounded p-2" />
+            </form>
         </>
     )
 }
